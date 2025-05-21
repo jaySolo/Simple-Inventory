e2bc8bb2c1d8ee103c178ad35c2d7f54a783a7da
Jamal J. A. Solomon <jj_solomon16@live.com>
2025-05-21 17:14:52 -0400

    created solution with projects

---
+++ tests/jsolo.simpleinventory.tests/SampleUnitTests.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+
+namespace jsolo.simpleinventory.tests
+{
+    public class SampleUnitTests
+    {
+        [Fact]
+        public void TestIsTrue() => Assert.True(true);
+        
+        [Fact]
+        public void TestIsFalse() => Assert.False(false);
+    }
+}
