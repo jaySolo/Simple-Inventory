6ae40df3a3a2577d54e8a0c791efff65512da8b0
Jamal J. A. Solomon <jj_solomon16@live.com>
2025-05-21 17:19:01 -0400

    added common/base classes and interfaces for 'sys' project

---
+++ src/jsolo.simpleinventory.sys/models/base/QueryFilterViewModel.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+
+namespace jsolo.simpleinventory.sys.models;
+
+
+
+public class QueryFilterViewModel
+{
+    public int PageSize { get; set; } = -1;
+
+    public int PageIndex { get; set; } = -1;
+
+    public string? SortBy { get; set; }
+
+    public string? OrderBy { get; set; }
+}
+
+
+
+public class QueryFilterResultsViewModel<T> where T : class
+{
+    public int Total { get; set; }
+
+    public IList<T> Items { get; set; } = new List<T>();
+}
